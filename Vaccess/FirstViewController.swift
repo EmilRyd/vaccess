47f8278c48e361a751dba97a3522007475a79753
Gustav Ryd <gustav@gustavrydsimac3.lan>
2019-09-11 22:31:47 +0200

    Initial Commit

---
+++ Vaccess/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Vaccess
+//
+//  Created by Gustav Ryd on 2019-09-11.
+//  Copyright © 2019 Ryd Corporation. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
